14746fcafd6c5c28663f71627e0a28332364d81a
Mihail Yonchev <Mihail Yonchev>
2020-02-08 16:34:51 +0000

    Adding java doc comments

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,24 @@
 package com.napier.sem;
 
+/**
+ * <h1>Class App</h1>
+ * The App class is used to ensure that implemented functionalities work.
+ *
+ * @author Daniela Todorova
+ * @author Miguel Bacharov
+ * @author Mihail Yonchev
+ * @author Valeri Vladimirov
+ * @version 0.1.0.1
+ * @since   2020-08-02
+ */
+
 public class App
 {
+    /**
+     * This is the main method
+     * @param args Unused.
+     * @return Nothing.
+     */
     public static void main(String[] args)
     {
         System.out.println("Boo yah!");
