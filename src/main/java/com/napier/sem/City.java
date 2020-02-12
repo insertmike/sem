c2750050749d26fed946a7eada82307665172475
migbash <20924663+migbash@users.noreply.github.com>
2020-02-12 00:57:26 +0000

    Adding Example of MySQL Query Update

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,23 @@
+package com.napier.sem;
+
+/**
+ * Represents the City Class
+ */
+public class City {
+
+    // City ID Number
+    public int id;
+
+    // City Name
+    public String name;
+
+    // City Country Code
+    public String country_code;
+
+    //City District
+    public String district;
+
+    //City Population
+    public int population;
+
+}
