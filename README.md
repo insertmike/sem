2e410aacb96a8da5e6b62b60b718fcebacf42d0e
insertmike <myonchev99@gmail.com>
2020-03-10 16:42:27 +0000

    Revert to last stable branch because of platform dependent files checked in.

---
+++ README.md
@@ -0,0 +1,102 @@
+# Software Engineer Methods Project :clipboard:	
+- Master Build Status : [![Build Status](https://travis-ci.org/insertmike/sem.svg?branch=master)](https://travis-ci.org/insertmike/sem)
+- Develop Build Status : [![Build Status](https://travis-ci.org/insertmike/sem.svg?branch=develop)](https://travis-ci.org/insertmike/sem)
+- License : [![LICENSE](https://img.shields.io/github/license/insertmike/sem.svg?style=flat-square)](https://github.com/insertmike/sem/blob/master/LICENSE)
+- Release : [![Releases](https://img.shields.io/github/release/insertmike/sem/all.svg?style=flat-square)](https://github.com/insertmike/sem/releases)
+
+---
+
+This report details information about the SEM Coursework Group Project for `Edinburgh Napier University`.
+
+*Started Project:* 21/01/2020
+
+*Project Members:* 
+- Valeri Vladimirov
+- Daniela Todorova
+- Mihail Yonchev
+- Miguel Bacharov
+
+*Finished Project:* TBD
+
+*Project Evaluation:* TBD
+
+---
+
+### Project Description:
+
+This is a group project as part of the Software Engineering Methods module.
+We are required to deliver a fully designed and implemented new system for an organisation to allow for
+easy access to population information.
+
+More Information on Coursework can be found [here](https://github.com/Kevin-Sim/set08103/tree/master/assessment).
+
+---
+
+
+### Reports
+
+---
+
+| ID  | Name          | Met           | Screenshot     |
+  | ---|:-------------:| -------------:| --------------:|
+  | 1  | Generate report with the population of a city |          Yes |       ![pic](./img/1.png)     |
+  | 2  | Generate report with the population of a district |          Yes |           ![pic](./img/2.png) |
+  | 3  | Generate report with the population of a country|          Yes |           ![pic](./img/3.png) |
+  | 4  | Generate report with the population of a region |          Yes |           ![pic](./img/4.png) |
+  | 5  | Generate report with the population of a continent |          Yes |           ![pic](./img/5.png)|
+  | 6  | Generate report with the population of the world |          Yes |           ![pic](./img/6.png) |
+  | 7  | Generate report with the population of people, people living in cities, and people not living in cities in each country |          Yes |           ![pic](./img/7.png) |
+  | 8  | Generate report with the population of people, people living in cities, and people not living in cities in each region |          Yes |           ![pic](./img/8.png) |
+  | 9  | Generate report with the population of people, people living in cities, and people not living in cities in each continent |          Yes |           ![pic](./img/9.png) |
+  
+  ---
+
+### Connecting to the MySQL Container (Step-by-step) guide:
+
+1. Pull/Fetch the latest master-branch version of the coursework.
+2. Make sure docker is set up, connected and running in the background.
+3. Run 'docker-compose.yml' file
+
+To gain direct access to the docker container, and execute SQL Queries within,
+do the following:
+1. Open your terminal inside this project directory.
+2. Run the following command: ```docker exec -it mysql bash -l```
+3. Followed by: ```mysql -u root -p```
+4. Enter your MySQL Container password
+5. Check that you can view your database tables by running ```SHOW DATABASES;```
+6. Done! :)
+7. You can now execute normal MySQL Queries just as if you were running a local MySQL instance using Workbench
+& Command Line Client.
+
+---
+
+### Running with Docker Compose:
+
+When running the app, make sure that you have:
+
+1. Packaged Maven Project Build Successfully,
+2. All docker containers & images have been deleted,
+3. Docker is up and running in your local machine,
+
+Now run the app using ```docker-compose.yml``` file.
+
+As an example, the app currently should do the following:
+
+1. Set up 2 docker containers in parallel,
+2. Execute the app container so that it detects, connects, queries & disconnects the
+   docker-db container successfully.
+3. You should see something like this:
+
+![image](https://user-images.githubusercontent.com/20924663/74292782-d7a67200-4d30-11ea-81db-f34bdb9ae940.png)
+
+---
+
+### Project Progress:
+
+Status | Report Type
+------ | -------
+☐  | Country
+☐  | City
+☐  | Capital City
+☐  | Population
+☐  | Other
---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,378 @@
+package com.napier.sem;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h1>Class App</h1>
+ * The App class is used to ensure and demonstrate implemented functionalities.
+ *
+ * @author Daniela Todorova
+ * @author Miguel Bacharov
+ * @author Mihail Yonchev
+ * @author Valeri Vladimirov
+ * @version 0.1.0.4
+ * @since   2020-08-02
+ */
+public class App
+{
+    /**
+     * This is the main method
+     * @param args Unused.
+     * @return Nothing
+     */
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Get Target City Record
+        City city = a.getCity(1);
+
+        // Display target result
+        a.displayCity(city);
+
+        // Get all cities
+        List<City> allCities = a.getAllCities();
+
+        System.out.println("Total cities: " + allCities.size());
+
+        System.out.println("First city in the list: " + allCities.get(0).toString());
+
+        // Get all countries
+        List<Country> allCountries = a.getAllCountries();
+
+        System.out.println("Total countries: " + allCountries.size());
+
+        Country firstCountryInList = allCountries.get(0);
+
+        System.out.println("First country in the list: " + firstCountryInList.toString());
+
+        System.out.println("City by largest population for: " + firstCountryInList.getName());
+
+        List<City> cityReport = a.getCitiesByLargestPopulationInCountry(firstCountryInList);
+
+        // The population of city Varna
+        System.out.println("The population of city: " + allCities.get(540).getName() + " is " + allCities.get(540).getPopulation());
+
+        // The population of district Varna
+        int citiesNumber = 0;
+        int totalPopulation = 0;
+        for(City city1: allCities){
+            if(city1.getDistrict().equals("Varna")){
+                totalPopulation += city1.getPopulation();
+                citiesNumber++;
+            }
+        }
+        System.out.println("The population of district: " + allCities.get(540).getDistrict() + " is " + totalPopulation + ", number of cities is " + citiesNumber);
+
+        // The population of country Bulgaria
+        System.out.println("The population of country: " + allCountries.get(22).getName() + " is " + allCountries.get(22).getPopulation());
+
+        // The population of region Eastern Europe
+        int totalPopulationRegion = 0;
+        for(Country country1: allCountries){
+            if(country1.getRegion().equals("Eastern Europe")){
+                totalPopulationRegion += country1.getPopulation();
+            }
+        }
+        System.out.println("The population of region: " + allCountries.get(22).getRegion() + " is " + totalPopulationRegion);
+
+        // The population of a continent Asia
+        long totalPopulationContinent = 0;
+        for(Country country1: allCountries){
+            if(country1.getContinent().equals("Asia")){
+                totalPopulationContinent += country1.getPopulation();
+            }
+        }
+        System.out.println("The population of continent: " + allCountries.get(1).getContinent() + " is " + totalPopulationContinent);
+
+        // The population of the world
+        long totalPopulationWorld = 0;
+        for(Country country1: allCountries){
+            totalPopulationWorld += country1.getPopulation();
+        }
+        System.out.println("The population of the world is: " + totalPopulationWorld);
+
+        // Population of people, people in cities and people not in cities in each country
+        for(Country country1: allCountries){
+            long populationInCity = 0;
+            for(City city1: allCities){
+                if(city1.getCountry_code().equals(country1.getISO3Code())){
+                    populationInCity += city1.getPopulation();
+                }
+            }
+            System.out.println("Population of people in " + country1.getName() + " is " + country1.getPopulation()
+                    + ". People living in cities are " + populationInCity + ". People not living in cities are " + (country1.getPopulation() - populationInCity) + ".");
+            break; // Removing the break will display for all countries. Break is added to limit data displayed so that we don't scroll 2 hours.
+        }
+
+        // Population of people, people in cities and people not in cities in each region
+        ArrayList<String> regions = new ArrayList<String>();
+        for(Country country1: allCountries){
+            if(!regions.contains(country1.getRegion())){
+                long populationRegion = 0;
+                long populationInCity = 0;
+                for (Country country2 : allCountries) {
+                    if (country1.getRegion().equals(country2.getRegion())) {
+                        populationRegion += country2.getPopulation();
+                        for (City city1 : allCities) {
+                            if (city1.getCountry_code().equals(country2.getISO3Code())) {
+                                populationInCity += city1.getPopulation();
+                            }
+                        }
+                    }
+                }
+                System.out.println("Population of people in " + country1.getRegion() + " is " + populationRegion
+                        + ". People living in cities are " + populationInCity + ". People not living in cities are " + (populationRegion - populationInCity) + ".");
+                regions.add(country1.getRegion());
+            }
+            break; // Removing the break will display for all countries. Break is added to limit data displayed so that we don't scroll 2 hours.
+        }
+
+        // Population of people, people in cities and people not in cities in each continent
+        ArrayList<String> continents = new ArrayList<String>();
+        for(Country country1: allCountries){
+            if(!continents.contains(country1.getContinent())){
+                long populationContinent = 0;
+                long populationInCity = 0;
+                for (Country country2 : allCountries) {
+                    if (country1.getContinent().equals(country2.getContinent())) {
+                        populationContinent += country2.getPopulation();
+                        for (City city1 : allCities) {
+                            if (city1.getCountry_code().equals(country2.getISO3Code())) {
+                                populationInCity += city1.getPopulation();
+                            }
+                        }
+                    }
+                }
+                System.out.println("Population of people in " + country1.getContinent() + " is " + populationContinent
+                        + ". People living in cities are " + populationInCity + ". People not living in cities are " + (populationContinent - populationInCity) + ".");
+                continents.add(country1.getContinent());
+            }
+            break; // Removing the break will display for all countries. Break is added to limit data displayed so that we don't scroll 2 hours.
+        }
+
+        for (City curr:
+             cityReport) {
+            System.out.println(curr);
+        }
+        
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    //Connection to MySQL database
+    private Connection con = null;
+
+    /**
+     * Connects to the mysql jdbc driver
+     * @return Nothing;
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            // Inform of Connectivity Processes
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+
+    /**
+     * // Disconnect from the MySQL database.
+     * @return Nothing;
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    /**
+     * All the cities from MySQL world database in the region of @param country organised by largest population
+     * @param country The region we aiming
+     * @return ArrayList<City>
+     */
+    public List<City> getCitiesByLargestPopulationInCountry(Country country){
+        List<City> cities = new ArrayList<>();
+        String countryCode = country.getISO3Code();
+        try{
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * FROM city WHERE city.CountryCode = '" + countryCode + "' ORDER BY city.Population DESC;";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            while(rset.next()){
+                City curr_city = new City(rset.getInt("ID"), rset.getString("Name"),  rset.getString("CountryCode"),rset.getString("District"),rset.getInt("Population"));
+                cities.add(curr_city);
+            }
+
+        } catch (Exception e){
+            return null;
+        }
+        return cities;
+    }
+
+    /**
+     * All countries from MySQL world database stored in ArrayList data structure.
+     * @return ArrayList<Country>
+     */
+    public List<Country> getAllCountries(){
+        List<Country> countries = new ArrayList<>();
+        try{
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * FROM country";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            while(rset.next()){
+                Country curr_country = new Country(rset.getString("Code"), rset.getString("Name"),
+                        rset.getString("Continent"),rset.getString("Region"),rset.getDouble("SurfaceArea"),
+                            rset.getInt("IndepYear"), rset.getInt("Population"), rset.getDouble("LifeExpectancy"),
+                                rset.getDouble("GNP"), rset.getDouble("GNPOld"), rset.getString("LocalName"),
+                                    rset.getString("GovernmentForm"), rset.getString("HeadOfState"),
+                                            rset.getInt("Capital"), rset.getString("Code2"));
+                countries.add(curr_country);
+            }
+
+        } catch (Exception e){
+            return null;
+        }
+        return countries;
+    }
+
+    /**
+     * All cities from MySQL world database stored in ArrayList data structure.
+     * @return ArrayList<City>
+     */
+    public List<City> getAllCities(){
+        List<City> cities = new ArrayList<>();
+        try{
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT * FROM city";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            while(rset.next()){
+                City curr_city = new City(rset.getInt("ID"), rset.getString("Name"),  rset.getString("CountryCode"),rset.getString("District"),rset.getInt("Population"));
+                cities.add(curr_city);
+            }
+
+        } catch (Exception e){
+            return null;
+        }
+        return cities;
+    }
+
+    /** Method GET Target City Data:
+     * @return City
+     */
+    public City getCity(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT ID, Name, CountryCode, District, Population "
+                            + "FROM city "
+                            + "WHERE ID = " + ID;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Return new employee if valid.
+
+            // Check one is returned
+            if (rset.next())
+            {
+                return new City(rset.getInt("ID"), rset.getString("Name"),  rset.getString("CountryCode"),rset.getString("District"),rset.getInt("Population"));
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    /** Method for Displaying Target Data
+     * @param city The city to be displayed
+     * @return Nothing
+     */
+    public void displayCity(City city)
+    {
+        if (city != null)
+        {
+            System.out.println(city.toString());
+        }
+    }
+}
